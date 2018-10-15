fb0f56e2e63fa8acd3f8dd1199571690e4d07e7a
lettuce.lu <32453095+lettucelu@users.noreply.github.com>
2018-10-15 19:12:24 +0800

    完成轉置矩陣的計算

--- Office/TransposeArray.py
+++ Office/TransposeArray.py
@@ -2,3 +2,16 @@
 # -*- coding:utf-8 -*-
 
 
+arrayA = [[1, 3, 5], [7, 9, 11], [13, 15, 17]]
+print(arrayA)
+
+def transpose(n):
+    arrayB = [[None] * 3 for row in range(3)]
+    print(arrayB)
+    for x in range(3):
+        for y in range(3):
+            arrayB[y][x] = arrayA[x][y]
+    print("arrayA 轉置後:" + str(arrayB))
+
+
+transpose(arrayA)
