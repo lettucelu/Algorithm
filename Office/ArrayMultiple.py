561365992fa3cf0ff95fb4154336176c8ce832d6
lettuce.lu <32453095+lettucelu@users.noreply.github.com>
2018-10-15 19:00:23 +0800

    完成兩矩陣相乘

--- Office/ArrayMultiple.py
+++ Office/ArrayMultiple.py
@@ -4,21 +4,27 @@
 
 def arrayMultiplied(arrayA, arrayB, arrayC, M, N, P):
 
-    print(arrayA, arrayB, arrayC, M, N, P)
+    #print(arrayA, arrayB, arrayC, M, N, P)
+    return arrayC
 
-
-arrayA = [['a11', 'a12', 'a13'], ['a21', 'a22', 'a23'], ['a31', 'a32', 'a33']]
-print(arrayA)
-arrayB = [['b11', 'b12', 'b13', 'b14'], ['b21', 'b22', 'b23', 'b24'], ['b31', 'b32', 'b33', 'b34'], ['b41', 'b42', 'b43', 'b44']]
-print(arrayB)
-arrayC = [[None] * 3 for row in range(4)]
-print(arrayC)
+arrayA = [[1, 3, 5], [7, 9, 11], [13, 15, 17]]
+print("arrayA:" + str(arrayA))
+arrayB = [[2, 4], [6, 8], [10, 12]]
+print("arrayB:" + str(arrayB))
+arrayC = [[0] * 2 for row in range(3)]
+temp = 0
+print("arrayC:" + str(arrayC))
+M = 3
+N = 3
+P = 2
 for x in range(3):
-    for y in range(3):
-        #print(x, y)
+    for z in range(2):
+        #print(z)
+        temp = 0
+        for y in range(3):
+            temp = temp + int(arrayA[x][y]) * int(arrayB[y][z])
+        arrayC[x][z] = temp
+        #print(arrayC[x][z])
+print(arrayC)
 
-        print("arrayA[%d]arrayA[%d]:%s" %(x, y, arrayA[x][y]))
-        print("arrayA[%d]arrayA[%d]:%s" % (x, y, arrayB[x][y]))
-        #arrayC[x][y] =
-
-#arrayMultiplied()
+arrayMultiplied(arrayA, arrayB, arrayC, M, N, P)
